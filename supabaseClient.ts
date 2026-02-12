98b5d67c8938321e532bb8b0d973d5427e37352d
ricojovemprojeto-dotcom <ricojovemprojeto@gmail.com>
2026-02-12 16:55:51 -0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://bdimsucrfuzdvfshsnry.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_f2EOzMlW6IFgBBbUZ7HtiA_5d_PVPIh';
+const SUPABASE_URL = 'https://lyduwedqzqphehvmqurs.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_cJ3rd1MVVOVvPk5Ikcl73g_OQPPlQG0';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
